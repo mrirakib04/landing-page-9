7fbc8431dd5f427dd678237d0e8d11da82181a6d
mrirakib04 <mdrakibulislam11928@gmail.com>
2025-07-16 02:14:43 +0600

    header added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import "./App.css";
+import Discount from "./components/Discount";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Holiday from "./components/Holiday";
+import Newsletter from "./components/Newsletter";
+import Offer from "./components/Offer";
+import Party from "./components/Party";
+import Portfolio from "./components/Portfolio";
 
 function App() {
-  return <div className="text-red-500">data</div>;
+  return (
+    <div className="flex flex-col items-center max-w-[1480px] mx-auto">
+      <Header></Header>
+      <Discount></Discount>
+      <Party></Party>
+      <Offer></Offer>
+      <Holiday></Holiday>
+      <Portfolio></Portfolio>
+      <Newsletter></Newsletter>
+      <Footer></Footer>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import bg from "./../assets/img/Banner Image.png";
+
+const Header = () => {
+  return (
+    <div className="container w-full py-10 px-5">
+      <div
+        style={{
+          background: `url(${bg})`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+        }}
+        className="w-full rounded-xl overflow-hidden"
+      >
+        <div className="w-full lg:py-40 md:py-24 sm:py-20 py-5 px-5  bg-gradient-to-b from-black/70 via-black/50 to-black/5 flex flex-col items-center justify-center text-center text-white gap-3">
+          <h3 className="md:text-2xl text-xl font-medium">Happy New Year</h3>
+          <h1 className="lg:text-7xl md:text-5xl sm:text-4xl text-3xl font-bold">
+            New Year Party <br />
+            Celebration
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
